cc50f8db44ec71231dfd31a982e1ff476ebb5b24
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2020-05-03 18:06:41 -0400

    Roll an arbitrary number of dice

--- src/Dice.elm
+++ src/Dice.elm
@@ -1,15 +1,32 @@
-module Dice exposing (oneDie, val)
+module Dice exposing (diceRoller, oneDie, val)
+
+import Random exposing (Generator)
 
 
 type OneDie
     = OneDie Int
 
 
+minDie : Int
+minDie =
+    1
+
+
+maxDie : Int
+maxDie =
+    6
+
+
 oneDie : Int -> OneDie
 oneDie n =
-    OneDie (clamp 1 6 n)
+    OneDie (clamp minDie maxDie n)
 
 
 val : OneDie -> Int
 val (OneDie n) =
     n
+
+
+diceRoller : Int -> Generator (List OneDie)
+diceRoller n =
+    Random.list n (Random.map oneDie (Random.int minDie maxDie))
--- tests/DiceTests.elm
+++ tests/DiceTests.elm
@@ -1,8 +1,9 @@
 module DiceTests exposing (..)
 
-import Dice exposing (oneDie, val)
+import Dice exposing (diceRoller, oneDie, val)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Random
 import Test exposing (..)
 
 
@@ -28,4 +29,28 @@ oneDieSuite =
                 List.map oneDie oneToSix
                     |> List.map val
                     |> Expect.equal oneToSix
+        ]
+
+
+diceRollerSuite : Test
+diceRollerSuite =
+    describe "The value of `diceRoller n` is a random generator returning a list of `n` random dice" <|
+        [ test "diceRoller 0 is a generator that always returns an empty list" <|
+            \_ ->
+                Random.step (diceRoller 0) (Random.initialSeed 42)
+                    |> Tuple.first
+                    |> List.map val
+                    |> Expect.equalLists []
+        , test "diceRoller 1 returns a list with one random die" <|
+            \_ ->
+                Random.step (diceRoller 1) (Random.initialSeed 42)
+                    |> Tuple.first
+                    |> List.map val
+                    |> Expect.equalLists [ 6 ]
+        , test "diceRoller 5 returns a list with five random dice" <|
+            \_ ->
+                Random.step (diceRoller 5) (Random.initialSeed 42)
+                    |> Tuple.first
+                    |> List.map val
+                    |> Expect.equalLists [ 1, 3, 1, 1, 6 ]
         ]
