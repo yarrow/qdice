f5c4aaecdbba4aa7222f09fee0cadbcba9b44d78
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2020-05-10 11:12:25 -0400

    Test that (inert initialScorePad) has no onClicks
    (All boxes b in all rows have b.onClick == Nothing)

--- src/Score.elm
+++ src/Score.elm
@@ -1,4 +1,4 @@
-module Score exposing (scoreOrder)
+module Score exposing (DisplayBox, DisplayRow, ScoreDisplay, ScorePad, inert, initialScorePad, scoreOrder)
 
 import Array exposing (Array)
 import Dice
@@ -9,3 +9,37 @@ import ScoreTags exposing (..)
 scoreOrder : List String
 scoreOrder =
     [ ones, twos, threes, fours, fives, sixes, upperTotal, bonus, threeOfAKind, fourOfAKind, fullHouse, smallStraight, largeStraight, fiveOfAKind, chance, total, weighted, grandTotal ]
+
+
+type alias ScoreDisplay =
+    List DisplayRow
+
+
+type alias DisplayRow =
+    { tag : String
+    , boxes : List DisplayBox
+    }
+
+
+type alias ScoreIndex =
+    String
+
+
+type alias DisplayBox =
+    { text : String
+    , onClick : Maybe ScoreIndex
+    }
+
+
+type ScorePad
+    = ScorePad
+
+
+initialScorePad : ScorePad
+initialScorePad =
+    ScorePad
+
+
+inert : ScorePad -> ScoreDisplay
+inert pad =
+    []
--- tests/ScoreTests.elm
+++ tests/ScoreTests.elm
@@ -7,9 +7,29 @@ import Set
 import Test exposing (..)
 
 
+onClickSuite : Test
+onClickSuite =
+    describe "Location of (Just) onClicks" <|
+        [ test "An inert ScoreDisplay has no onClicks" <|
+            \_ ->
+                let
+                    inactive =
+                        inert initialScorePad
+
+                    boxes =
+                        List.concatMap .boxes inactive
+
+                    clicks =
+                        List.map .onClick boxes
+                in
+                List.all (\it -> it == Nothing) clicks
+                    |> Expect.true "Found an onClick in (inert initialScorePad)"
+        ]
+
+
 allTags : Test
 allTags =
-    test "scoreOrder has all the tags" <|
+    test "scoreDisplay has all the tags" <|
         \_ ->
             let
                 allTheTags =
