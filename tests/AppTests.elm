1af5f1836f706ef141cfa8dc5f9c1f0e7bdc3830
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2020-05-18 14:21:18 -0400

    Use one overall test (suite) per test module
    ... so we can expose just that test instead of (..), and still add
    tests freely, while letting elm-analyser complain about ExposeAll.
    
    Might split into separate suites again in the future.

--- tests/AppTests.elm
+++ tests/AppTests.elm
@@ -1,4 +1,4 @@
-module AppTests exposing (..)
+module AppTests exposing (appTests)
 
 import App exposing (Model, Msg(..), initialModel, update, view)
 import Dice exposing (DiceBoard(..))
@@ -9,20 +9,6 @@ import Random
 import Test exposing (..)
 import Test.Html.Query as Query
 import Test.Html.Selector exposing (attribute, id, tag, text)
-
-
-initialModelSuite : Test
-initialModelSuite =
-    describe "Properties of initial model" <|
-        [ test "We start with no dice" <|
-            \_ ->
-                initialModel.dice
-                    |> Expect.equal Nothing
-        , test "We start with three rolls available" <|
-            \_ ->
-                initialModel.remainingRolls
-                    |> Expect.equal 3
-        ]
 
 
 type alias PreDice =
@@ -44,123 +30,131 @@ modelAfterFirstRoll =
     update (GotDice randomPipsList) initialModel |> Tuple.first
 
 
-updateSuite : Test
-updateSuite =
-    describe "Properties of update" <|
-        let
-            keep =
-                ( 1, Keep )
+appTests : Test
+appTests =
+    describe "Tests for App.elm" <|
+        [ describe "Properties of initial model" <|
+            [ test "We start with no dice" <|
+                \_ ->
+                    initialModel.dice
+                        |> Expect.equal Nothing
+            , test "We start with three rolls available" <|
+                \_ ->
+                    initialModel.remainingRolls
+                        |> Expect.equal 3
+            ]
+        , describe "Properties of update" <|
+            let
+                keep =
+                    ( 1, Keep )
 
-            reroll =
-                ( 1, Reroll )
+                reroll =
+                    ( 1, Reroll )
 
-            keepAll =
-                [ keep, keep, keep, keep, keep ]
-        in
-        [ test "RollDice doesn't change the model" <|
-            \_ ->
-                update RollDice initialModel
-                    |> Tuple.first
-                    |> Expect.equal initialModel
-        , test "RollDice sends Cmd.none if remainingRolls is 0" <|
-            \_ ->
-                update RollDice { modelAfterFirstRoll | remainingRolls = 0 }
-                    |> Tuple.second
-                    |> Expect.equal Cmd.none
-        , test "RollDice sends Cmd.none if there are no dice to be rerolled" <|
-            \_ ->
-                update RollDice (modelWithDice keepAll)
-                    |> Tuple.second
-                    |> Expect.equal Cmd.none
+                keepAll =
+                    [ keep, keep, keep, keep, keep ]
+            in
+            [ test "RollDice doesn't change the model" <|
+                \_ ->
+                    update RollDice initialModel
+                        |> Tuple.first
+                        |> Expect.equal initialModel
+            , test "RollDice sends Cmd.none if remainingRolls is 0" <|
+                \_ ->
+                    update RollDice { modelAfterFirstRoll | remainingRolls = 0 }
+                        |> Tuple.second
+                        |> Expect.equal Cmd.none
+            , test "RollDice sends Cmd.none if there are no dice to be rerolled" <|
+                \_ ->
+                    update RollDice (modelWithDice keepAll)
+                        |> Tuple.second
+                        |> Expect.equal Cmd.none
 
-        {-
-              , test "In the initial model, (GotDice someDice) installs (Just someDice) as the model's .dice value" <|
-                  \_ ->
-                      modelWithRandomDice.dice
-                          |> Expect.equal (Just Dice.makeDiceBoard randomPipsList)
-           , test "Initially, all dice have nextRoll == Keep" <|
-               \_ ->
-                   List.all (\d -> Die.nextRoll d == Keep) randomPipsList
-                       |> Expect.true "Initially, all dice have an NextRoll of Keep"
-        -}
-        , test "`DieFlipped 0` causes the 0th die to flip its NextRoll status" <|
-            \_ ->
-                let
-                    rerollFirst =
-                        Just (Dice.makeDiceBoard [ reroll, keep, keep, keep, keep ])
-                in
-                update (DieFlipped 0) (modelWithDice keepAll)
-                    |> Tuple.first
-                    |> .dice
-                    |> Expect.equal rerollFirst
-        , test "Incoming dice replace dice to be rerolled" <|
-            \_ ->
-                let
-                    startingDice =
-                        [ keep, reroll, keep, reroll, keep ]
+            {-
+                  , test "In the initial model, (GotDice someDice) installs (Just someDice) as the model's .dice value" <|
+                      \_ ->
+                          modelWithRandomDice.dice
+                              |> Expect.equal (Just Dice.makeDiceBoard randomPipsList)
+               , test "Initially, all dice have nextRoll == Keep" <|
+                   \_ ->
+                       List.all (\d -> Die.nextRoll d == Keep) randomPipsList
+                           |> Expect.true "Initially, all dice have an NextRoll of Keep"
+            -}
+            , test "`DieFlipped 0` causes the 0th die to flip its NextRoll status" <|
+                \_ ->
+                    let
+                        rerollFirst =
+                            Just (Dice.makeDiceBoard [ reroll, keep, keep, keep, keep ])
+                    in
+                    update (DieFlipped 0) (modelWithDice keepAll)
+                        |> Tuple.first
+                        |> .dice
+                        |> Expect.equal rerollFirst
+            , test "Incoming dice replace dice to be rerolled" <|
+                \_ ->
+                    let
+                        startingDice =
+                            [ keep, reroll, keep, reroll, keep ]
 
-                    incomingDice =
-                        [ 2, 3 ]
+                        incomingDice =
+                            [ 2, 3 ]
 
-                    resultingDice =
-                        Just (Dice.makeDiceBoard [ keep, ( 2, Keep ), keep, ( 3, Keep ), keep ])
-                in
-                update (GotDice incomingDice) (modelWithDice startingDice)
-                    |> Tuple.first
-                    |> .dice
-                    |> Expect.equal resultingDice
-        , test "After the first roll, we have 2 rolls remaining" <|
-            \_ ->
-                update (GotDice randomPipsList) initialModel
-                    |> Tuple.first
-                    |> .remainingRolls
-                    |> Expect.equal 2
-        ]
-
-
-viewSuite : Test
-viewSuite =
-    describe "Properties of view" <|
-        [ test "The app has a 'Roll Dice' button" <|
-            \_ ->
-                view initialModel
-                    |> Query.fromHtml
-                    |> Query.find [ tag "button", id "roll-dice" ]
-                    |> Query.has [ text "Roll Dice" ]
-        , test "We start with no dice" <|
-            \_ ->
-                view initialModel
-                    |> Query.fromHtml
-                    |> Query.findAll [ tag "img" ]
-                    |> Query.count (Expect.equal 0)
-        , test "When there are dice, there are five dice" <|
-            \_ ->
-                view modelAfterFirstRoll
-                    |> Query.fromHtml
-                    |> Query.findAll [ tag "img" ]
-                    |> Query.count (Expect.equal 5)
-        , test "There are five dice rows" <|
-            \_ ->
-                view modelAfterFirstRoll
-                    |> Query.fromHtml
-                    |> Query.findAll [ tag "tr", attribute <| Attr.class "dice-row" ]
-                    |> Query.count (Expect.equal 5)
-        , test "There are five dice rows in the initial Model too" <|
-            \_ ->
-                view initialModel
-                    |> Query.fromHtml
-                    |> Query.findAll [ tag "tr", attribute <| Attr.class "dice-row" ]
-                    |> Query.count (Expect.equal 5)
-        , test "The inital model shows 3 remainingRolls" <|
-            \_ ->
-                view initialModel
-                    |> Query.fromHtml
-                    |> Query.find [ tag "caption" ]
-                    |> Query.has [ text "3 rolls remaining" ]
-        , test "After the first roll, we show two rolls remaining" <|
-            \_ ->
-                view modelAfterFirstRoll
-                    |> Query.fromHtml
-                    |> Query.find [ tag "caption" ]
-                    |> Query.has [ text "2 rolls remaining" ]
+                        resultingDice =
+                            Just (Dice.makeDiceBoard [ keep, ( 2, Keep ), keep, ( 3, Keep ), keep ])
+                    in
+                    update (GotDice incomingDice) (modelWithDice startingDice)
+                        |> Tuple.first
+                        |> .dice
+                        |> Expect.equal resultingDice
+            , test "After the first roll, we have 2 rolls remaining" <|
+                \_ ->
+                    update (GotDice randomPipsList) initialModel
+                        |> Tuple.first
+                        |> .remainingRolls
+                        |> Expect.equal 2
+            ]
+        , describe "Properties of view" <|
+            [ test "The app has a 'Roll Dice' button" <|
+                \_ ->
+                    view initialModel
+                        |> Query.fromHtml
+                        |> Query.find [ tag "button", id "roll-dice" ]
+                        |> Query.has [ text "Roll Dice" ]
+            , test "We start with no dice" <|
+                \_ ->
+                    view initialModel
+                        |> Query.fromHtml
+                        |> Query.findAll [ tag "img" ]
+                        |> Query.count (Expect.equal 0)
+            , test "When there are dice, there are five dice" <|
+                \_ ->
+                    view modelAfterFirstRoll
+                        |> Query.fromHtml
+                        |> Query.findAll [ tag "img" ]
+                        |> Query.count (Expect.equal 5)
+            , test "There are five dice rows" <|
+                \_ ->
+                    view modelAfterFirstRoll
+                        |> Query.fromHtml
+                        |> Query.findAll [ tag "tr", attribute <| Attr.class "dice-row" ]
+                        |> Query.count (Expect.equal 5)
+            , test "There are five dice rows in the initial Model too" <|
+                \_ ->
+                    view initialModel
+                        |> Query.fromHtml
+                        |> Query.findAll [ tag "tr", attribute <| Attr.class "dice-row" ]
+                        |> Query.count (Expect.equal 5)
+            , test "The inital model shows 3 remainingRolls" <|
+                \_ ->
+                    view initialModel
+                        |> Query.fromHtml
+                        |> Query.find [ tag "caption" ]
+                        |> Query.has [ text "3 rolls remaining" ]
+            , test "After the first roll, we show two rolls remaining" <|
+                \_ ->
+                    view modelAfterFirstRoll
+                        |> Query.fromHtml
+                        |> Query.find [ tag "caption" ]
+                        |> Query.has [ text "2 rolls remaining" ]
+            ]
         ]
--- tests/DiceTests.elm
+++ tests/DiceTests.elm
@@ -1,4 +1,4 @@
-module DiceTests exposing (..)
+module DiceTests exposing (diceTests)
 
 import Dice exposing (DiceBoard(..), diceRoller, fiveDice, flipNth)
 import Die exposing (NextRoll(..))
@@ -8,38 +8,36 @@ import Random
 import Test exposing (..)
 
 
-diceRollerSuite : Test
-diceRollerSuite =
-    describe "The value of `diceRoller n` is a random generator returning a list of `n` random dice" <|
-        [ test "diceRoller 0 is a generator that always returns an empty list" <|
-            \_ ->
-                Random.step (diceRoller 0) (Random.initialSeed 42)
-                    |> Tuple.first
-                    |> Expect.equalLists []
-        , test "diceRoller 1 returns a list with one random die" <|
-            \_ ->
-                Random.step (diceRoller 1) (Random.initialSeed 42)
-                    |> Tuple.first
-                    |> Expect.equalLists [ 6 ]
-        , test "diceRoller 5 returns a list with five random dice" <|
-            \_ ->
-                Random.step (diceRoller 5) (Random.initialSeed 42)
-                    |> Tuple.first
-                    |> Expect.equalLists [ 1, 3, 1, 1, 6 ]
-        , fuzz (intRange Random.minInt Random.maxInt) "fiveDice always returns five dice" <|
-            \seed ->
-                Random.step fiveDice (Random.initialSeed seed)
-                    |> Tuple.first
-                    |> List.length
-                    |> Expect.equal 5
-        ]
-
-
-flipNthSuite : Test
-flipNthSuite =
-    describe "flipNth n dice performs flipNextRoll on the nth element of dice" <|
-        [ test "flipNth 0" <|
-            \_ ->
-                flipNth 0 (Dice.makeDiceBoard [ ( 1, Keep ), ( 2, Reroll ) ])
-                    |> Expect.equal (Dice.makeDiceBoard [ ( 1, Reroll ), ( 2, Reroll ) ])
+diceTests : Test
+diceTests =
+    describe "Tests for Dice.elm" <|
+        [ describe "The value of `diceRoller n` is a random generator returning a list of `n` random dice" <|
+            [ test "diceRoller 0 is a generator that always returns an empty list" <|
+                \_ ->
+                    Random.step (diceRoller 0) (Random.initialSeed 42)
+                        |> Tuple.first
+                        |> Expect.equalLists []
+            , test "diceRoller 1 returns a list with one random die" <|
+                \_ ->
+                    Random.step (diceRoller 1) (Random.initialSeed 42)
+                        |> Tuple.first
+                        |> Expect.equalLists [ 6 ]
+            , test "diceRoller 5 returns a list with five random dice" <|
+                \_ ->
+                    Random.step (diceRoller 5) (Random.initialSeed 42)
+                        |> Tuple.first
+                        |> Expect.equalLists [ 1, 3, 1, 1, 6 ]
+            , fuzz (intRange Random.minInt Random.maxInt) "fiveDice always returns five dice" <|
+                \seed ->
+                    Random.step fiveDice (Random.initialSeed seed)
+                        |> Tuple.first
+                        |> List.length
+                        |> Expect.equal 5
+            ]
+        , describe "flipNth n dice performs flipNextRoll on the nth element of dice" <|
+            [ test "flipNth 0" <|
+                \_ ->
+                    flipNth 0 (Dice.makeDiceBoard [ ( 1, Keep ), ( 2, Reroll ) ])
+                        |> Expect.equal (Dice.makeDiceBoard [ ( 1, Reroll ), ( 2, Reroll ) ])
+            ]
         ]
